9dae3efbeb782389b00a18f7b8c5f8d40b5e7685
David <musicman585@hotmail.com>
2024-03-13 23:29:14 -0700

    finished calcure task sync script

--- task_sync.sh
+++ task_sync.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-set -xv
 
 # Greetings this script was made to sync tasks from calcure to a email client Maildir
 # So that you can have your calcure task list update seemlessly between computers
 
-# Please update the two path variables below before running
+# Please update the three variables below before running
 
 email="david@computerisms.ca"
 mail_tasks_dir="${XDG_DATA_HOME:-$HOME/.local/share}/mail/$email/Tasks"
 calcure_task_file="${XDG_CONFIG_HOME:-$HOME/.config}/calcure/tasks.csv"
+temp_mail_task=$(mktemp)
+temp_calcure_task=$(mktemp)
+
+#checks for a trailing / and deletes it
+[[ "$mail_tasks_dir" =~ .*"/"$ ]] && mail_tasks_dir=${mail_tasks_dir%/}
 
 # For the script to work properly it expects the input emails to be in
 # calcure's task.csv file format. I may update it to be more accommodating in
@@ -19,10 +23,6 @@ calcure_task_file="${XDG_CONFIG_HOME:-$HOME/.config}/calcure/tasks.csv"
 # below format to a email template so I only need to edit the template as new
 # message copy paste new tasks as I need them. The script needs to have the
 # start/end tags to run properly.
-
-# to edit/delete tasks in your email use the -m switch to delete all emails in
-# your tasks folder and afterwards the script will send you a new email with the
-# current calcure task.csv file
 
 #Please follow the below template in your email (without the comment of course):
 
@@ -35,42 +35,38 @@ calcure_task_file="${XDG_CONFIG_HOME:-$HOME/.config}/calcure/tasks.csv"
 ######### Leave everything else below #############
 ###################################################
 
-if ! command -v inotifywait &> /dev/null
-then
-	echo "please install inotify-tools to use this script"
-	exit 1
-fi
-
-
 [ ! -d $mail_tasks_dir ] && { echo "invalid directory path. please update the: mail_tasks_dir variable";exit 1; }
 [ ! -f $calcure_task_file ] && { echo "invalid file path. please update the: calcure_task_file variable";exit 1; }
 
-temp=$(mktemp)
-trap 'rm -fr $temp' 0 2 3 15
-
-HelpMenu() {
-echo "please use one of the following:
-
--r	overwrite your calcure tasks file from your email tasks directory
--m	Merge together multiple task emails into a single file for easier management
-"
-exit
-}
-
 RefreshTask() {
-mv $calcure_task_file $calcure_task_file.backup
-truncate -s 0 $calcure_task_file
-
 #matches maildir's cur,new,tmp folders
 for mail in $mail_tasks_dir/???/*
 do
+
 	# awk '/start/{p=1;next}/end/{p=0}p' $mail | sed '/^$/d'| sed 's/\t/--/g' | sed "s/\([[:alnum:] \-]*\)/$deadline,\"\1\",$importance/g" >> $calcure_task_file
-	awk '/^start$/{p=1;next}/^end$/{p=0}p' $mail | sed '/^[[:space:]]$/d' >> $calcure_task_file
+
+	awk '/^start$/{p=1;next}/^end$/{p=0}p' $mail | sed '/^[[:space:]]*$/d' >> $temp_mail_task
 done
-sed -i '/^$/d' $calcure_task_file
+
+cat $calcure_task_file > $temp_calcure_task
+truncate -s 0 $calcure_task_file
+
+if diff -q $temp_mail_task $temp_calcure_task
+then
+	echo "no changes"
+	cat $temp_calcure_task > $calcure_task_file
+	exit
+else
+	echo -e "\t===Mail_Tasks===\t\t\t\t\t\t===Local_Tasks===\n"
+	sdiff -s $temp_mail_task $temp_calcure_task -o $calcure_task_file
+
+fi
+
 }
 
 MergNewTasks() {
+pass $email 2>&1 > /dev/null || { echo "Please unlock GPG keyring to sync";exit; }
+RefreshTask
 for mail in $mail_tasks_dir/???/*
 do
 	rm -f $mail
@@ -81,15 +77,4 @@ sleep 5
 mbsync -q $email
 }
 
-[ -z "$1" ] && HelpMenu
-[ -z "$2" ] || { echo "You can only use one option at a time" ; HelpMenu; }
-
-case "$1" in
-	-r)
-		RefreshTask;;
-	-m)
-		MergNewTasks;;
-	*)
-		echo "Option $1 not recognized"
-		HelpMenu;;
-esac
+MergNewTasks
