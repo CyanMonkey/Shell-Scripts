b20fe1b03cc63168a753e8ea78cf6c8bd6e2ea6b
CyanMonkey <33214247+CyanMonkey@users.noreply.github.com>
2025-08-12 09:10:06 -0700

    Create windows_path_check.sh
    new script :)

---
+++ windows_path_check.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+#This Script is used to scan a windows file system for files & directories that are past the windows 256 character limit
+#It generates 2 files, one for unfiltered filesystem output and another for the paths over the character limit
+
+
+read -p "please supply a directory path: " directory_root
+
+[ -d "$directory_root" ] && cd $directory_root || { echo "not valid path please try again"; exit; }
+
+tempfile=$(mktemp)
+tempfile2=$(mktemp)
+max_depth=256
+output="$HOME/path_output.txt"
+problem_files="$HOME/path_problem_files.txt"
+
+[ -f $output ] && { rm -f $output; }
+[ -f $problem_files ] && { rm -f $problem_files; }
+
+
+du -ch . | sed 's/ /spaceholder/g'| awk '{print $2}' >> $tempfile2
+cat $tempfile2 | while read path; do echo "$path" | wc -c ; done >> $tempfile
+cat $tempfile2 | paste $tempfile - > $output
+
+sed -i 's/spaceholder/ /g' $output
+
+while read line
+do
+        length=$(echo $line | awk '{print $1}')
+        if [ "$length" -gt "$max_depth" ]
+        then
+                echo "$line" >> $problem_files
+        fi
+done < $output
+
+rm -f $tempfile $tempfile2
+
+echo "view the reports under $HOME"
